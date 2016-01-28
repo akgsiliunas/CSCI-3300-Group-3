7ebfd64ef05ae8ac944898f862a1514ab5d1b8e0
Liu Chen <chenliuleo@gmail.com>
2016-01-28 00:47:38 -0600

    Created Enemies, set enemies to random.

---
+++ Semester Project/Assets/__Scripts/Main.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Main : MonoBehaviour
+{
+    static public Main S;
+
+    public GameObject[] prefabEnemies;
+    public float enemySpawnPerSecond = 0.5f;
+    public float enemySpawnPadding = 1.5f;
+
+    public bool _;
+
+    public float enemySpawnRate;
+
+    void Awake()
+    {
+        S = this;
+        Utils.SetCameraBounds(this.GetComponent<Camera>());
+        enemySpawnRate = 1f / enemySpawnPerSecond;
+        Invoke("SpawnEnemy", enemySpawnRate);
+    }
+
+    public void SpawnEnemy()
+    {
+        int ndx = Random.Range(0, prefabEnemies.Length);
+        GameObject go = Instantiate(prefabEnemies[ndx]) as GameObject;
+        Vector3 pos = Vector3.zero;
+        float xMin = Utils.camBounds.min.x + enemySpawnPadding;
+        float xMax = Utils.camBounds.max.x - enemySpawnPadding;
+        pos.x = Random.Range(xMin, xMax);
+        pos.y = Utils.camBounds.max.y + enemySpawnPadding;
+        go.transform.position = pos;
+        Invoke("SpawnEnemy", enemySpawnRate);
+    }
+}
