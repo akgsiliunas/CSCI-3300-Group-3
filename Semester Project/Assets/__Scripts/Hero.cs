1e357b8333dbf28f5b5d2f703925a88ff1e560dd
Yijie Wang <ywang85@slu.edu>
2016-02-02 17:43:15 -0600

    update

--- Semester Project/Assets/__Scripts/Hero.cs
+++ Semester Project/Assets/__Scripts/Hero.cs
@@ -1,22 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class _Hero2 : MonoBehaviour {
-	static public _Hero2 S2;
+public class Hero : MonoBehaviour {
+	static public Hero S;
 	public float speed=30;
 	public float rollMult = -45;
 	public float pitchMult = 30;
 	public float shieldLevel=1;
 	public bool _____________;
+	public delegate void WeaponFireDelegate();
+	public WeaponFireDelegate fireDelegate;
 
 	void Awake(){
-		S2 = this;
+		S = this;
 	}
 	// Use this for initialization
 	void Start () {
-
+		
 	}
-
+	
 	// Update is called once per frame
 	void Update () {
 		float xAxis = Input.GetAxis ("Horizontal");
@@ -26,6 +28,10 @@ public class _Hero2 : MonoBehaviour {
 		pos.y += yAxis * speed * Time.deltaTime;
 		transform.position = pos;
 
+
 		transform.rotation = Quaternion.Euler (yAxis * pitchMult, xAxis * rollMult, 0);
+
+		if (Input.GetAxis ("Jump") == 1 && fireDelegate != null)
+			fireDelegate ();
 	}
 }
---
+++ Semester Project/Assets/__Scripts/Projectile.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+	[SerializeField]
+	private WeaponType _type;
+
+	public WeaponType type{
+		get{
+			return _type;
+		}
+		set{
+			SetType (value);
+		}
+	}
+
+	void Awake(){
+		InvokeRepeating ("CheckOffscreen", 2f, 2f);
+	}
+
+	public void SetType(WeaponType eType){
+		_type=eType;
+		WeaponDefinition def = Main.GetWeaponDefinition (_type);
+		GetComponent<Renderer>().material.color = def.projectileColor;
+	}
+
+	void CheckOffscreen(){
+		if (Utils.ScreenBoundsCheck (GetComponent<Collider> ().bounds, BoundsTest.offScreen) != Vector3.zero)
+			Destroy (this.gameObject);
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
