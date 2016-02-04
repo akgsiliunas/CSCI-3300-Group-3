ff4c3cd6ab1cbba1f0c11bccf4470e52ac63d248
Corey <caldwell@slu.edu>
2016-02-03 20:44:54 -0600

    Update Hero.cs

--- Semester Project/Assets/__Scripts/Hero.cs
+++ Semester Project/Assets/__Scripts/Hero.cs
@@ -6,6 +6,7 @@ public class Hero : MonoBehaviour {
 	public float speed=30;
 	public float rollMult = -45;
 	public float pitchMult = 30;
+	public float gameRestartDelay = 2f;
 
     [SerializeField]
     private float _shieldLevel=1;
@@ -98,6 +99,7 @@ public class Hero : MonoBehaviour {
             if (value < 0)
             {
                 Destroy(this.gameObject);
+                Main.S.DelayedRestart(gameRestartDelay);	// Tells Main.S to restart the game after a delay
             }
         }
     }
