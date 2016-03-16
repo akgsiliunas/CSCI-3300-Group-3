79ddca4cdd64546fe7718e7372a350e7d2435302
Yijie Wang <ywang85@slu.edu>
2016-03-15 22:03:59 -0500

    print a to z

---
+++ Semester Project/Assets/__Scripts/AtoZ.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AtoZ : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		printAtoZ ("", 3);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void printAtoZ(string s,int size){
+		if (size == 0) {
+			Debug.Log (s);
+			return;
+		}
+		for (char ch='a';ch<='z';ch++){
+			printAtoZ((string)(s+ch),size-1);
+		}
+	}
+
+
+}
