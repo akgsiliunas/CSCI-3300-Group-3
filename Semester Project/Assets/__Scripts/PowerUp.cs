8c6e6ba292394b2ff45140132b54cd58aa571877
Antanas Siliunas <asiliun2@slu.edu>
2016-04-16 08:31:09 -0500

    Added scaled down and particle system effects to power up class. Cleaned up commented-out code in power up class.

--- Semester Project/Assets/__Scripts/PowerUp.cs
+++ Semester Project/Assets/__Scripts/PowerUp.cs
@@ -4,76 +4,55 @@ using DG.Tweening;
 
 public class PowerUp : MonoBehaviour {
 
-
-    public Vector2 rotMinMax = new Vector2(15, 90);
-    //public Vector2 driftMinMax = new Vector2(.25f, 2);
-    public float lifeTime = 6f;
-    public float fadeTime = 4f;
-
-    public bool __________________;
+    public float lifeSpan = 6f;
+    public float scaleDownTime = 1f;
+    private float timeChange = 0f;
 
     public WeaponType type;
     public GameObject cube;
-    // public TextMesh letter;
     private GameObject weaponGraphic;
-
     public GameObject weaponGraphicTransform;
 
     public Vector3 rotPerSecond;
-    public float birthTime;
+    public Vector2 rotMinMax = new Vector2(15, 90);
+
+    public ParticleSystem absorbtion;
 
     void Awake()
     {
-        //cube = transform.Find("Cube").gameObject;
-
-        //letter = GetComponent<TextMesh>();
-
-        //Vector3 vel = Random.onUnitSphere;
-
-        //vel.z = 0;
-        //vel.Normalize();
-        //vel *= Random.Range(driftMinMax.x, driftMinMax.y);
-
-        //GetComponent<Rigidbody>().velocity = vel;
-
         transform.rotation = Quaternion.identity;
-
         rotPerSecond = new Vector3(Random.Range(rotMinMax.x, rotMinMax.y), Random.Range(rotMinMax.x, rotMinMax.y), Random.Range(rotMinMax.x, rotMinMax.y));
-
-        //InvokeRepeating("CheckOffscreen", 2f, 2f);
-
-        birthTime = Time.time;
     }
 
 	void Update () {
 
+        if (timeChange > lifeSpan)
+        {
+            timeChange = 0f;
+
+            foreach (Transform child in transform)
+            {
+                if (child.GetComponent<Collider>() != null)
+                    child.GetComponent<Collider>().enabled = false;
+            }
+
+            transform.DOScale(0f, scaleDownTime);
+            Destroy(this.gameObject, scaleDownTime);
+        }
+        else
+            timeChange += Time.deltaTime;
+
+
         cube.transform.rotation = Quaternion.Euler(rotPerSecond * Time.time);
-
-        float u = (Time.time - (birthTime + lifeTime)) / fadeTime;
-
-        if (u >= 1)
-        {
-            Destroy (this.gameObject);
-            return;
-        }
-
-        if (u > 0)
-        {
-            // USE FADE ALL CHILDREN CODE FROM CAPSTONE
-        }
 	}
 
     public void SetType( WeaponType wt)
     {
         WeaponDefinition def = Main.GetWeaponDefinition(wt);
-        //letter.text = def.letter;
 
         weaponGraphic = def.weaponGraphic;
         GameObject clone = Instantiate(weaponGraphic, weaponGraphicTransform.transform.position, weaponGraphicTransform.transform.rotation) as GameObject;
-
-
         clone.transform.parent = weaponGraphicTransform.transform.parent;
-
         clone.transform.position = weaponGraphicTransform.transform.position;
  
         type = wt;
@@ -81,12 +60,15 @@ public class PowerUp : MonoBehaviour {
 
     public void AbsorbedBy (GameObject target)
     {
-        Destroy(this.gameObject);
+        foreach (Transform child in transform)
+        {
+            if (child.GetComponent<Collider>() != null)
+                child.GetComponent<Collider>().enabled = false;
+            if (child.GetComponent<MeshRenderer>() != null)
+                child.GetComponent<MeshRenderer>().enabled = false;
+        }
+        absorbtion.Play();
+
+        Destroy(this.gameObject, 1f);
     }
-
-
-
-
-
-
 }
